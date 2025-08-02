cbfb6ba2956ab150fa30abd499917516dd49ba08
siderilla <siderilla11@gmail.com>
2025-08-02 22:27:42 +0200

    formgroup formcontrol

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+
+  taskForm = new FormGroup({
+    title: new FormControl(''),
+    priority: new FormControl(''),
+    expiration: new FormControl(''),
+    completation: new FormControl(false)
+  });
+
+  priorityArray = [
+    {label: 'Alta', value: 1},
+    {label: 'Media', value: 2},
+    {label: 'Bassa', value: 3}
+  ]
 
 }
